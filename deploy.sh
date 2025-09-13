eb8776d2d4d2640d63ad228fc47e1d21c28dfcb0
Sunny Vanderwall <sunsunwall@gmail.com>
2025-09-14 01:02:05 +0200

    Deploying latest changes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Pushing to GitHub..."
+git add .
+git commit -m "Deploying latest changes"
 git push origin main
 echo "Checking build status..."
 gcloud builds list --filter="source.repoSource.repoName=yet_another_pipeline AND status=WORKING OR status=SUCCESS OR status=FAILURE" --limit=1 --format="table(id,status,startTime,duration)"
